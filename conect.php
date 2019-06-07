78c75110706d3587dec6d12febc9fea9afa5355f
jsegurar3431 <jsegurar3431@universidadean.edu.co>
2019-06-07 13:46:11 -0500

    Se actualiza la conexión para la BD phpMyadmin

---
+++ conect.php
@@ -0,0 +1,23 @@
+<?php
+
+$usuario=$_post ['usuario'];
+$clave=$_post ['password'];
+
+$conexion=mysqli_connect ("db4free.net","sicet_adweb","Ean1234Sicet","sicet_web");
+$consulta="SELECT * FROM SICET_USUARIO WHERE usuario='$usuario' and clave='$password'";
+$resultado=mysqli_query ($conexion,$consulta);
+
+$filas=mysqli_num_rows ($resultado);
+
+if ($filas>0){
+    header("location:menupal.html");
+           }
+
+else{
+    echo "Error de credenciales";
+}
+mysqli_free_result($resultado);
+mysqli_close($conexion);
+
+
+?>
